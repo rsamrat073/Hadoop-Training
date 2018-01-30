c9d063b93ed68e6523ae5839c4f9af986f584871
sroy28 <sroy28@BLRSROY2838484.sapient.com>
2018-01-30 15:27:21 +0530

    final 12

--- Assignment3/src/main/java/client/EmployeeDataAnalyzer.java
+++ Assignment3/src/main/java/client/EmployeeDataAnalyzer.java
@@ -19,9 +19,8 @@ public class EmployeeDataAnalyzer {
 
 	public EmployeeDataAnalyzer() {
 
-		JavaRDD<String> empsAll = SparkContext.getContext().textFile(new File(
-				"src\\main\\resources\\employees.txt")
-						.getAbsolutePath());
+		JavaRDD<String> empsAll = SparkContext.getContext()
+				.textFile(new File("src\\main\\resources\\employees.txt").getAbsolutePath());
 		String header = empsAll.first();
 		emps = empsAll.filter(data -> !data.equals(header)).map(line -> {
 			String[] p = line.split(",");
@@ -40,15 +39,15 @@ public class EmployeeDataAnalyzer {
 	}
 
 	public void getEmployeeSalesPerYear() {
-		
-		//Total Sales per Month
-		emps.mapToPair(data->new Tuple2<>(data.getEmpNumber(),data.getSlaesPerMonth()))
-		.reduceByKey((x,y)->x.floatValue()+y.floatValue()).collect().forEach(s->System.out.println(s));
-		
-		//Total Sales per Year
-		emps.mapToPair(data->new Tuple2<>(data.getEmpNumber(),data.getSlaesPerYear()))
-		.reduceByKey((x,y)->x.floatValue()+y.floatValue()).collect().forEach(s->System.out.println(s));
-	
+
+		// Total Sales per Month
+		emps.mapToPair(data -> new Tuple2<>(data.getEmpNumber(), data.getSlaesPerMonth()))
+				.reduceByKey((x, y) -> x.floatValue() + y.floatValue()).collect().forEach(s -> System.out.println(s));
+
+		// Total Sales per Year
+		emps.mapToPair(data -> new Tuple2<>(data.getEmpNumber(), data.getSlaesPerYear()))
+				.reduceByKey((x, y) -> x.floatValue() + y.floatValue()).collect().forEach(s -> System.out.println(s));
+
 	}
 
 	public void calculateTotalSalePerEmployee() {
@@ -57,32 +56,43 @@ public class EmployeeDataAnalyzer {
 
 	}
 
-	public void calculateAverageEmployeeMonthlySales() {
-		JavaPairRDD<Integer, Tuple2<Float,Integer>> countSalesPerMonth = emps.mapToPair(data -> new Tuple2<Integer, Float>(data.getEmpNumber(), data.getSlaesPerMonth()))
+	public JavaPairRDD<Integer, Float> calculateAverageEmployeeMonthlySales() {
+		JavaPairRDD<Integer, Tuple2<Float, Integer>> countSalesPerMonth = emps
+				.mapToPair(data -> new Tuple2<Integer, Float>(data.getEmpNumber(), data.getSlaesPerMonth()))
 				.mapValues(d -> new Tuple2<>(d, 1));
 
-		JavaPairRDD<Integer, Float> avgSales = countSalesPerMonth.reduceByKey((x,y)->new Tuple2<>(x._1()+y._1(),x._2()+y._2()))
-		.mapToPair(d->new Tuple2<>(d._1(),d._2()._1()/d._2()._2()));
-		avgSales.collect().forEach(s->System.out.println(s));
-		
+		JavaPairRDD<Integer, Float> avgSales = countSalesPerMonth
+				.reduceByKey((x, y) -> new Tuple2<>(x._1() + y._1(), x._2() + y._2()))
+				.mapToPair(d -> new Tuple2<>(d._1(), d._2()._1() / d._2()._2()));
+		avgSales.collect().forEach(s -> System.out.println(s));
+		return avgSales;
+
 	}
-	
+
 	public void maxMinSalesOverYear() {
+
+		JavaPairRDD<Integer, Tuple2<Integer, Float>> actualSalesPerYear = emps.mapToPair(
+				data -> new Tuple2<>(data.getEmpNumber(), new Tuple2<>(data.getEmpNumber(), data.getSlaesPerYear())));
+		actualSalesPerYear.mapValues(t -> new Tuple2<>(t._2(), 1.0F))
+				.reduceByKey(
+						(x, y) -> new Tuple2<>(x._1() > y._1() ? x._1() : y._1(), x._1() > y._1() ? y._1() : x._1()))
+				.collect().forEach(s -> System.out.println(s));
+
+	}
+
+	public void maxMinAvgSalesOverMonth() {
+
+		JavaPairRDD<Integer, Float> avgSales = calculateAverageEmployeeMonthlySales();
 		
-		
-		JavaPairRDD<Integer, Tuple2<Integer,Float>> actualSalesPerYear = 
-				emps.mapToPair(data->new Tuple2<>(data.getEmpNumber(),new Tuple2<>(data.getEmpNumber(),data.getSlaesPerYear())));
-		actualSalesPerYear.mapValues(t->new Tuple2<>(t._2(),1.0F)).
-	reduceByKey((x,y)->new Tuple2<>(x._1()>y._1()?x._1():y._1(),x._1()>y._1()?y._1():x._1()))
-		.collect().forEach(s->System.out.println(s));
-		
-		
-	
+		System.out.println("MAX-->"+avgSales.mapToPair(data->new Tuple2<>(data._2(),data._1()))
+		.sortByKey(false).first()+" MIN-->"+avgSales.mapToPair(data->new Tuple2<>(data._2(),data._1()))
+		.sortByKey(true).first());
+
 	}
 
 	public static void main(String[] args) {
 
-		new EmployeeDataAnalyzer().getEmployeeSalesPerYear();
+		new EmployeeDataAnalyzer().maxMinAvgSalesOverMonth();
 
 	}
 
